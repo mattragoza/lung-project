051614403da9ad1f1ed0b876ea5a91a5f0e40f00
mattragoza <mtr22@pitt.edu>
2023-10-25 16:05:55 -0400

    tabs to spaceS

--- register.py
+++ register.py
@@ -4,19 +4,19 @@ import SimpleITK as sitk
 
 
 class Registration(object):
-	'''
-	A registration problem using SimpleElastix.
-	'''
-	def __init__(self, fixed_array, moving_array, transform='rigid'):
-		self.params = sitk.GetDefaultParameterMap(transform)
-		self.filter = sitk.ElastixImageFilter()
-		self.filter.SetParameterMap(self.params)
-		self.filter.SetFixedImage(fixed_image)
-		self.filter.SetMovingImage(moving_image)
+    '''
+    A registration problem using SimpleElastix.
+    '''
+    def __init__(self, fixed_array, moving_array, transform='rigid'):
+        self.params = sitk.GetDefaultParameterMap(transform)
+        self.filter = sitk.ElastixImageFilter()
+        self.filter.SetParameterMap(self.params)
+        self.filter.SetFixedImage(fixed_image)
+        self.filter.SetMovingImage(moving_image)
 
-	def execute(self, verbose=False):
-		self.filter.Execute()
+    def execute(self, verbose=False):
+        self.filter.Execute()
 
 
 def image_from_xarray(array):
-	image = sitk.Image()
+    image = sitk.Image()
