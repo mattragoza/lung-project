44d9d7122f532097fb902ab486649c688160cd2c
mattragoza <mtr22@pitt.edu>
2023-11-01 16:58:51 -0400

    registration work

--- register.py
+++ register.py
@@ -5,22 +5,98 @@ import SimpleITK as sitk
 
 class Registration(object):
     '''
-    A registration problem using SimpleElastix.
+    Intensity-based registration using SimpleElastix.
     '''
-    def __init__(self, fixed, moving, transform='rigid'):
-        self.params = sitk.GetDefaultParameterMap(transform)
-        self.filter = sitk.ElastixImageFilter()
-        self.filter.SetParameterMap(self.params)
-        self.fixed = fixed
-        self.moving = moving
+    def __init__(self, learning_rate=1.0, n_iterations=100):
+        self.method = sitk.ImageRegistrationMethod()
+        self.method.SetMetricAsMeanSquares()
+        self.method.SetInterpolator(sitk.sitkLinear)
+        self.method.SetOptimizerAsRegularStepGradientDescent(
+            learningRate=learning_rate,
+            numberOfIterations=n_iterations,
+            gradientMagnitudeTolerance=1e-8,
+            minStep=1e-4
+        )
+        self.method.SetOptimizerScalesFromPhysicalShift()
+        self.method.SetShrinkFactorsPerLevel([4, 2, 1])
+        self.method.SetSmoothingSigmasPerLevel([2, 1, 0])
+        self.method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
+        self.method.AddCommand(sitk.sitkIterationEvent, self.print_metric)
 
-    def get_result(self):
-        return xarray_from_image(self.filter.GetResultImage())
+    @property
+    def iteration(self):
+        return self.method.GetOptimizerIteration()
 
-    def execute(self, verbose=False):
-        self.filter.SetFixedImage(image_from_xarray(self.fixed))
-        self.filter.SetMovingImage(image_from_xarray(self.moving))
-        self.filter.Execute()
+    @property
+    def metric(self):
+        return self.method.GetMetricValue()
+
+    @property
+    def optimizer_position(self):
+        return self.method.GetOptimizerPosition()
+
+    @property
+    def optimizer_scale(self):
+        return self.method.GetOptimizerScales()
+
+    def print_metric(self):
+        if self.iteration == 0:
+            print('Estimated scales: ', self.optimizer_scale)
+        print(f'[{self.iteration:4}] {self.metric}')
+
+    def execute(self, fixed: xr.DataArray, moving: xr.DataArray, type='rigid'):
+        fixed_image = image_from_xarray(fixed)
+        moving_image = image_from_xarray(moving)
+        initial = getattr(Deformation, type)(fixed_image, moving_image)
+        self.method.SetInitialTransform(initial.transform)
+        final_transform = self.method.Execute(fixed_image, moving_image)
+        return Deformation(final_transform)
+
+
+class Deformation(object):
+
+    def __init__(self, transform):
+        self.transform = transform
+
+    @classmethod
+    def rigid(cls, fixed, moving):
+        transform = sitk.CenteredTransformInitializer(
+            fixed_image,
+            moving_image,
+            sitk.Euler3DTransform(),
+            sitk.CenteredTransformInitializerFilter.GEOMETRY
+        )
+        return cls(transform)
+
+    @classmethod
+    def affine(cls, fixed, moving):
+        transform = sitk.CenteredTransformInitializer(
+            fixed_image,
+            moving_image,
+            sitk.AffineTransform(3),
+            sitk.CenteredTransformInitializerFilter.GEOMETRY
+        )
+        return cls(transform)
+
+    @classmethod
+    def bspline(cls, fixed, moving):
+        transform = sitk.BSplineTransform(3, 3)
+        return cls(transform)
+
+    def invert(self):
+        inverse = self.transform.GetInverse()
+        return Deformation(transform=inverse)
+
+    def compose(self, other):
+        composite = sitk.CompositeTransform(3)
+        composite.AddTransform(self.transform)
+        composite.AddTransform(other.transform)
+        return Deformation(transform=composite)
+
+    def apply(self, points):
+        return np.array([
+            self.transform.TransformPoint(p) for p in points
+        ])
 
 
 def image_from_xarray(array):
