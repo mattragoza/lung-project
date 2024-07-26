3c9b639298c82d871265075259117a7297e2193f
mattragoza <mtr22@pitt.edu>
2024-07-26 12:00:29 -0400

    intrapatient registration working

--- project/register.py
+++ project/register.py
@@ -10,10 +10,10 @@ def image_from_xarray(array):
     assert array.dims == ('x', 'y', 'z')
     data_T = array.transpose('z', 'y', 'x').data
     image = sitk.GetImageFromArray(data_T)
-    xres = float(array.x[1] - array.x[0])
-    yres = float(array.y[1] - array.y[0])
-    zres = float(array.z[1] - array.z[0])
-    image.SetSpacing((xres, yres, zres))
+    x_res = float(array.x[1] - array.x[0])
+    y_res = float(array.y[1] - array.y[0])
+    z_res = float(array.z[1] - array.z[0])
+    image.SetSpacing((x_res, y_res, z_res))
     x0 = float(array.x[0])
     y0 = float(array.y[0])
     z0 = float(array.z[0])
@@ -22,6 +22,9 @@ def image_from_xarray(array):
 
 
 def xarray_from_image(image):
+    '''
+    Convert a 3D SITK image to an xarray.
+    '''
     assert image.GetDimension() == 3
     assert image.GetNumberOfComponentsPerPixel() == 1
     data = sitk.GetArrayFromImage(image).T
@@ -35,60 +38,149 @@ def xarray_from_image(image):
             'x': origin[0] + np.arange(shape[0]) * resolution[0],
             'y': origin[1] + np.arange(shape[1]) * resolution[1],
             'z': origin[2] + np.arange(shape[2]) * resolution[2]
-        },
-        name='result'
+        }
     )
     return array
+
+
+def angle_to_versor(angle_degrees):
+    angle_radians = np.deg2rad(angle_degrees)
+    return np.sin(angle_radians / 2.0)
 
 
 def register_image(
     image_mov,
     image_fix,
+    transform='similarity',
+    center='geometry',
+    metric='MI',
+    scale_init=1.0,
+    num_scale_steps=0,
+    scale_step_size=0.1,
     learning_rate=1.0,
-    num_iterations=100,
+    num_iterations=0,
     print_every=10,
-    transform='similarity'
 ):
-    if transform == 'affine':
+    '''
+    Perform non-deformable image registration using SITK.
+
+    This is intended for intra-patient registration as a
+    preprocessing step to generate images with the same
+    shape and similar FOV for input to deep learning model.
+
+    Args:
+        image_mov: SITK moving image
+        image_fix: SITK fixed image
+        transform: Type of transformation model
+            Options are 'affine', 'similarity', or 'rigid'
+        center: Type of center for initialization
+            Options are 'geometric' or 'moments'
+        metric: Metric for image registration
+            Options are 'MSE' or 'MI'
+    Returns:
+        transform: SITK transform object
+    '''
+    transform_type = transform
+    if transform_type == 'affine':
         transform = sitk.AffineTransform(3)
-    elif transform == 'similarity':
+    elif transform_type == 'similarity':
         transform = sitk.Similarity3DTransform()
-    elif transform == 'rigid':
+        transform.SetScale(scale_init)
+    elif transform_type == 'rigid':
         transform = sitk.Euler3DTransform()
 
+    if center == 'geometry':
+        center = sitk.CenteredTransformInitializerFilter.GEOMETRY
+    elif center == 'moments':
+        center = sitk.CenteredTransformInitializerFilter.MOMENTS
+
+    # initialize center of rotation parameters
     transform = sitk.CenteredTransformInitializer(
-        image_fix, image_mov, transform,
-        sitk.CenteredTransformInitializerFilter.MOMENTS
+        image_fix, image_mov, transform, center
     )
-    method = sitk.ImageRegistrationMethod()
-    method.SetMetricAsMeanSquares()
-    method.SetInterpolator(sitk.sitkLinear)
-    method.SetOptimizerAsGradientDescent(
+
+    reg_method = sitk.ImageRegistrationMethod()
+    reg_method.SetInterpolator(sitk.sitkLinear)
+
+    if metric == 'MSE':
+        reg_method.SetMetricAsMeanSquares()
+    elif metric == 'MI':
+        reg_method.SetMetricAsMattesMutualInformation()
+
+    # initialize scale parameter by grid search
+    if num_scale_steps > 0: 
+        print('Start exhaustive search...')
+        num_angle_steps = 0 # very slow if nonzero
+        angle_step_size = 0.1 #angle_to_versor(10)
+        reg_method.SetInitialTransform(transform)
+        reg_method.SetOptimizerAsExhaustive(
+            numberOfSteps=[
+                num_angle_steps,
+                num_angle_steps,
+                num_angle_steps,
+                0, 0, 0,
+                num_scale_steps
+            ]
+        )
+        reg_method.SetOptimizerScales([
+            angle_step_size,
+            angle_step_size,
+            angle_step_size,
+            0.0, 0.0, 0.0,
+            scale_step_size
+        ])
+
+        def print_iteration():
+            position = reg_method.GetOptimizerPosition()
+            metric = reg_method.GetMetricValue()
+            print(f'{position} metric = {metric:.4f}')
+
+        reg_method.AddCommand(sitk.sitkIterationEvent, print_iteration)
+        transform = reg_method.Execute(image_fix, image_mov)
+   
+    # then perform iterative optimization
+    print('Start iterative refinement...')
+    reg_method.SetInitialTransform(transform)
+    reg_method.SetOptimizerAsGradientDescent(
         learningRate=learning_rate,
         numberOfIterations=num_iterations,
         convergenceMinimumValue=1e-6,
         convergenceWindowSize=10,
     )
-    method.SetOptimizerScalesFromPhysicalShift()
-    method.SetShrinkFactorsPerLevel([4, 2, 1])
-    method.SetSmoothingSigmasPerLevel([2, 1, 0])
-    method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
-    method.SetInitialTransform(transform)
+    reg_method.SetOptimizerScalesFromPhysicalShift()
+    reg_method.SetShrinkFactorsPerLevel([4, 2, 1])
+    reg_method.SetSmoothingSigmasPerLevel([2, 1, 0])
+    reg_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
 
     def print_iteration():
-        level = method.GetCurrentLevel() + 1
-        iteration = method.GetOptimizerIteration()
-        error = method.GetMetricValue()
+        level = reg_method.GetCurrentLevel() + 1
+        iteration = reg_method.GetOptimizerIteration()
+        metric = reg_method.GetMetricValue()
         if iteration % print_every == 0:
-            print(f'[level {level}|iteration {iteration}] error = {error:.4f}')
+            print(f'[level {level}|iteration {iteration}] metric = {metric:.4f}')
 
-    method.AddCommand(sitk.sitkIterationEvent, print_iteration)
+    reg_method.RemoveAllCommands()
+    reg_method.AddCommand(sitk.sitkIterationEvent, print_iteration)
+    transform = reg_method.Execute(image_fix, image_mov)
 
-    transform = method.Execute(image_fix, image_mov)
     return transform
 
 
 def transform_image(image_mov, image_fix, transform, default=0):
+    '''
+    Apply transformation to image using SITK.
+
+    Args:
+        image_mov: SITK moving image
+            Input image to transform/resample.
+        image_fix: SITK fixed image
+            Determines output sampling grid.
+        transform: SITK transform object
+        default: Value for out-of-domain sampling
+    Returns:
+        image_warp: SITK moving image after applying
+            transform and resampling on fixed image grid.
+    '''
     resampler = sitk.ResampleImageFilter()
     resampler.SetReferenceImage(image_fix)
     resampler.SetInterpolator(sitk.sitkBSpline)
@@ -110,4 +202,5 @@ def transform_array(array_mov, array_fix, *args, **kwargs):
     image_fix = image_from_xarray(array_fix.astype(float))
     image_warp = transform_image(image_mov, image_fix, *args, **kwargs)
     array_warp = xarray_from_image(image_warp).astype(array_mov.dtype)
+    array_warp.name = array_mov.name
     return array_warp
