b4e3da0bd5c3e04033e50f13d20b010bba7540ed
mattragoza <mtr22@pitt.edu>
2024-08-21 11:46:30 -0400

    run training nb with custom interpolation, seems better

--- project/interpolate.py
+++ project/interpolate.py
@@ -5,39 +5,95 @@ import fenics as fe
 import torch_fenics
 
 
-def image_to_dofs(image, resolution, V):
+def interpolate_image(image, points, radius=1, resolution=1.0, sigma=1.0):
+    '''
+    Image interpolation method that takes a weighted sum
+    of the image values in the neighborhood of each point.
+
+    Args:
+        image: (C, X, Y, Z) image tensor
+        points: (N, 3) sampling points
+        radius: neighborhood size (int)
+        resolution: image resolution (float)
+        sigma: Gaussian parameter (float)
+    '''
+    C, X, Y, Z = image.shape
+    N, D = points.shape
+    
+    if isinstance(radius, int):
+        radius = [radius] * D
+    
+    zeros = torch.zeros(D, device=image.device)
+    shape = torch.as_tensor([X, Y, Z], device=image.device)
+    resolution = torch.as_tensor(resolution, device=image.device) 
+    
+    x_offsets = torch.arange(-radius[0], radius[0], device=image.device) + 1
+    y_offsets = torch.arange(-radius[1], radius[1], device=image.device) + 1
+    z_offsets = torch.arange(-radius[2], radius[2], device=image.device) + 1
+    
+    offsets = torch.meshgrid(x_offsets, y_offsets, z_offsets)
+    offsets = torch.stack(offsets, dim=-1).reshape(-1, D)
+    
+    interpolated_values = torch.zeros(N, C, device=image.device)
+
+    for i, point in enumerate(points):
+        nearest_voxel = (point / resolution).floor().long()
+        
+        neighbor_voxels = nearest_voxel.unsqueeze(0) + offsets
+        neighbor_voxels = neighbor_voxels.clamp(min=zeros, max=shape-1).long()
+        
+        neighbor_values = image[
+            :,
+            neighbor_voxels[:,0],
+            neighbor_voxels[:,1],
+            neighbor_voxels[:,2],
+        ].T
+        
+        neighbor_points = neighbor_voxels * resolution.unsqueeze(0)
+        distance = torch.norm(neighbor_points - point.unsqueeze(0), dim=1)
+        
+        weights = torch.exp(-(distance**2) / (2*sigma**2))
+        weighted_sum = (weights.unsqueeze(1) * neighbor_values).sum(dim=0) 
+
+        interpolated_values[i] = weighted_sum / weights.sum()
+        
+    return interpolated_values
+
+
+def image_to_dofs(image, resolution, V, radius, sigma):
     '''
     Args:
-        image: (n_c, n_x, n_y, n_z) torch.Tensor
+        image: (C, X, Y, Z) torch.Tensor
         V: fenics.FunctionSpace
-            defined on (mesh_size, 3) coordinates
+            with dofs on (N, 3) coordinates
     Returns:
-        dofs: (batch_size, mesh_size, n_channels) torch.Tensor
+        dofs: (N, C) torch.Tensor
     '''
-    n_c, n_x, n_y, n_z = image.shape
+    if V.num_sub_spaces() == 0 and image.ndim == 3:
+        image = image.unsqueeze(0)
+
+    C, X, Y, Z = image.shape
     
-    coords = V.tabulate_dof_coordinates()
+    dof_coords = V.tabulate_dof_coordinates()
     if V.num_sub_spaces() > 0:
-        coords = coords[::V.num_sub_spaces()]
+        dof_coords = dof_coords[::V.num_sub_spaces(),:]
     
-    mesh_size, n_dims = coords.shape
+    N, D = dof_coords.shape
 
-    coords = torch.as_tensor(coords, dtype=image.dtype, device=image.device)
+    dof_coords = torch.as_tensor(dof_coords, dtype=image.dtype, device=image.device)
 
-    shape = torch.as_tensor([n_x, n_y, n_z], dtype=image.dtype, device=image.device)
-    resolution = torch.as_tensor(resolution, dtype=image.dtype, device=image.device)
-    extent = (shape - 1) * resolution
-
-    dofs = F.grid_sample(
-        input=image[None,...].permute(0,1,4,3,2), # cxyz -> bczyx
-        grid=(coords[None,None,None,...] / extent) * 2 - 1,
-        align_corners=True
-    ).to(torch.float64)
+    dofs = interpolate_image(
+        image=image,
+        points=dof_coords,
+        resolution=resolution,
+        radius=radius,
+        sigma=sigma,
+    ).double()
 
     if V.num_sub_spaces() > 0:
-        return dofs.view(n_c, mesh_size).permute(1,0)
+        return dofs.view(N, C)
     else:
-        return dofs.view(mesh_size)
+        return dofs.view(N)
 
 
 def dofs_to_image(dofs, V, image_shape, resolution):
