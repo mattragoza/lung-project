b30753a7a07ecf3c38dac6a638e8eff977aa95d4
mattragoza <mtr22@pitt.edu>
2024-10-13 18:01:01 -0400

    fix pytorch cuda not available

--- tests/test_gpu.py
+++ tests/test_gpu.py
@@ -1,3 +1,7 @@
 import torch
-print(torch.cuda.is_available())
+print('CUDA available: ', torch.cuda.is_available())
+print('CUDA version:   ', torch.version.cuda)
+print('Device count:   ', torch.cuda.device_count())
+print('Device name:    ', torch.cuda.get_device_name(0) if torch.cuda.is_available() else 'No device found')
+print('CUDNN enabled:  ', torch.backends.cudnn.enabled)
 
