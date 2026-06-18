4136701b21b3e545d0a66cda3c8f6e5559b19a21
mattragoza <mtr22@pitt.edu>
2026-06-18 16:31:27 -0400

    rename _ensure_output to run_stage

--- project/preprocessing/pipelines.py
+++ project/preprocessing/pipelines.py
@@ -1,11 +1,18 @@
 from typing import List, Dict, Tuple, Optional, Any
+from pathlib import Path
 
 from ..core import utils
 
 from . import stages
 
 
-def _ensure_output(func, *args, **kwargs) -> Tuple[bool, Any]:
+def run_stage(
+    func,
+    *args,
+    output_path: Path = None,
+    force: bool = False,
+    **kwargs
+) -> Tuple[bool, Any]:
     '''
     Call function if output path does not exist.
 
@@ -16,17 +23,16 @@ def _ensure_output(func, *args, **kwargs) -> Tuple[bool, Any]:
         bool: True if the function was called.
         Any: Return value from function call.
     '''
-    output_path = kwargs.get('output_path')
+    if output_path is None:
+        raise ValueError(f'{func.__name__} requires output_path')
 
-    run_stage = True
-    if output_path is not None:
-        run_stage = not output_path.exists()
+    output_path.parent.mkdir(parents=True, exist_ok=True)
 
-    if run_stage:
-        utils.log(f'INFO: {output_path} missing; Running stage {func.__name__}')
+    if force or not output_path.exists():
+        log(f'INFO: {output_path} missing; Running stage {func.__name__}')
         return True, func(*args, **kwargs)
 
-    utils.log(f'INFO: {output_path} exists; Skipping stage {func.__name__}')
+    log(f'INFO: {output_path} exists; Skipping stage {func.__name__}')
     return False, None
 
 
@@ -41,34 +47,34 @@ def preprocess_shapenet(ex, config):
     base_seed = config.pop('random_seed', 0)
     subj_seed = utils.make_seed(base_seed, ex.subject)
 
-    _ensure_output( # binary mask
+    run_stage( # binary mask
         stages.preprocess_binary_mask,
         mask_path=ex.paths['source_mask'],
         mesh_path=ex.paths['source_mesh'],
         output_path=ex.paths['binary_mask'],
         config=config.get('binary_mask', {})
     )
-    _ensure_output( # surface mesh
+    run_stage( # surface mesh
         stages.preprocess_surface_mesh,
         input_path=ex.paths['source_mesh'],
         output_path=ex.paths['surface_mesh'],
         config=config.get('surface_mesh', {})
     )
-    _ensure_output( # region mask
+    run_stage( # region mask
         stages.create_mesh_region_mask,
         mask_path=ex.paths['binary_mask'],
         mesh_path=ex.paths['source_mesh'],
         output_path=ex.paths['region_mask'],
         config=config.get('region_mask', {})
     )
-    _ensure_output( # volume mesh
+    run_stage( # volume mesh
         stages.generate_tetrahedral_mesh,
         mask_path=ex.paths['region_mask'],
         output_path=ex.paths['volume_mesh'],
         config=config.get('volume_mesh', {}),
         random_seed=subj_seed
     )
-    _ensure_output( # material mask
+    run_stage( # material mask
         stages.create_material_mask,
         mask_path=ex.paths['region_mask'],
         output_path=ex.paths['material_mask'],
@@ -78,7 +84,7 @@ def preprocess_shapenet(ex, config):
         config=config.get('material_mask', {}),
         random_seed=subj_seed
     )
-    _ensure_output( # material mesh
+    run_stage( # material mesh
         stages.create_material_fields,
         regions_path=ex.paths['region_mask'],
         materials_path=ex.paths['material_mask'],
@@ -86,21 +92,21 @@ def preprocess_shapenet(ex, config):
         output_path=ex.paths['material_mesh'],
         config=config.get('material_mesh', {})
     )
-    _ensure_output( # input image
+    run_stage( # input image
         stages.generate_volumetric_image,
         mask_path=ex.paths['material_mask'],
         output_path=ex.paths['input_image'],
         config=config.get('image_generation', {}),
         random_seed=subj_seed
     )
-    _ensure_output( # interp mesh
+    run_stage( # interp mesh
         stages.interpolate_image_fields,
         image_path=ex.paths['input_image'],
         mesh_path=ex.paths['material_mesh'],
         output_path=ex.paths['interp_mesh'],
         config=config.get('image_interpolation', {})
     )
-    _ensure_output( # simulate mesh
+    run_stage( # simulate mesh
         stages.simulate_displacement_field,
         mesh_path=ex.paths['interp_mesh'],
         output_path=ex.paths['simulate_mesh'],
@@ -122,7 +128,7 @@ def preprocess_copdgene(ex, config):
 
     for state in ['init_state', 'curr_state']:
 
-        _ensure_output( # resampled_image
+        run_stage( # resampled_image
             stages.resample_image_spacing,
             ref_path=ex.paths['ref_state']['source_image'],
             input_path=ex.paths[state]['source_image'],
@@ -134,7 +140,7 @@ def preprocess_copdgene(ex, config):
 
     for state in ['init_state', 'curr_state']:
 
-        _ensure_output( # combined_mask
+        run_stage( # combined_mask
             stages.create_segmentation_masks,
             input_path=ex.paths[state]['resampled_image'],
             segment_dir=ex.paths[state]['segment_dir'],
@@ -142,7 +148,7 @@ def preprocess_copdgene(ex, config):
             config=config.get('image_segmentation', {})
         )
 
-        _ensure_output( # region_map
+        run_stage( # region_map
             stages.create_multi_region_map,
             input_dir=ex.paths[state]['segment_dir'],
             output_path=ex.paths[state]['region_map'],
@@ -151,7 +157,7 @@ def preprocess_copdgene(ex, config):
 
     # ----- image registration -----
 
-    _ensure_output( # disp_field
+    run_stage( # disp_field
         stages.register_displacement_field,
         fixed_image=ex.paths['init_state']['resampled_image'],
         moving_image=ex.paths['curr_state']['resampled_image'],
@@ -163,14 +169,14 @@ def preprocess_copdgene(ex, config):
 
     # ----- mesh construction -----
 
-    _ensure_output( # region_mesh
+    run_stage( # region_mesh
         stages.generate_tetrahedral_mesh,
         mask_path=ex.paths['region_map'],
         output_path=ex.paths['region_mesh'],
         config=config.get('mesh_generation', {})
     )
 
-    _ensure_output( # interp_mesh
+    run_stage( # interp_mesh
         stages.interpolate_mesh_fields,
         mesh_path=ex.paths['region_mesh'],
         image_path=ex.paths['input_image'],
@@ -192,7 +198,7 @@ def preprocess_emory4dct(ex, config):
 
     for state in ['ref_state', 'init_state', 'curr_state']:
 
-        _ensure_output( # converted_image
+        run_stage( # converted_image
             stages.convert_image_to_nifti,
             input_path=ex.paths[state]['source_image'],
             output_path=ex.paths[state]['converted_image'],
@@ -203,7 +209,7 @@ def preprocess_emory4dct(ex, config):
 
     for state in ['init_state', 'curr_state']:
 
-        _ensure_output( # resampled_image
+        run_stage( # resampled_image
             stages.resample_image_spacing,
             ref_path=ex.paths['ref_state']['converted_image'],
             input_path=ex.paths[state]['converted_image'],
@@ -215,7 +221,7 @@ def preprocess_emory4dct(ex, config):
 
     for state in ['init_state', 'curr_state']:
 
-        _ensure_output( # combined_mask
+        run_stage( # combined_mask
             stages.create_segmentation_masks,
             input_path=ex.paths[state]['resampled_image'],
             segment_dir=ex.paths[state]['segment_dir'],
@@ -223,7 +229,7 @@ def preprocess_emory4dct(ex, config):
             config=config.get('image_segmentation', {})
         )
 
-        _ensure_output( # region_map
+        run_stage( # region_map
             stages.create_multi_region_map,
             input_dir=ex.paths[state]['segment_dir'],
             output_path=ex.paths[state]['region_map'],
@@ -232,7 +238,7 @@ def preprocess_emory4dct(ex, config):
 
     # ----- image registration -----
 
-    _ensure_output( # disp_field
+    run_stage( # disp_field
         stages.register_displacement_field,
         fixed_image=ex.paths['init_state']['resampled_image'],
         moving_image=ex.paths['curr_state']['resampled_image'],
@@ -244,14 +250,14 @@ def preprocess_emory4dct(ex, config):
 
     # ----- mesh construction -----
 
-    _ensure_output( # region_mesh
+    run_stage( # region_mesh
         stages.generate_tetrahedral_mesh,
         mask_path=ex.paths['region_map'],
         output_path=ex.paths['region_mesh'],
         config=config.get('mesh_generation', {})
     )
 
-    _ensure_output( # interp_mesh
+    run_stage( # interp_mesh
         stages.interpolate_mesh_fields,
         mesh_path=ex.paths['region_mesh'],
         image_path=ex.paths['input_image'],
@@ -259,6 +265,11 @@ def preprocess_emory4dct(ex, config):
         output_path=ex.paths['interp_mesh'],
         config=config.get('mesh_interpolation', {})
     )
+
+
+
+def preprocess_phantom(ex, config):
+    raise NotImplementedError
 
 
 def preprocess_bmc4dct(ex, config):
