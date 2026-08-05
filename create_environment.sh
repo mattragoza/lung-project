4cf42ee0314b791bce33627b72cf697ab193cd7c
mattragoza <mtr22@pitt.edu>
2026-08-04 22:57:43 -0400

    fix environment bug

--- create_environment.sh
+++ create_environment.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 mamba env create -f environment.yml -v
+mamba run -n warp pip install "nnunetv2>=2.3.1"
 mamba run -n warp pip install "TotalSegmentator>=2.5" --no-deps
-mamba run -n warp pip install "nnunetv2>=2.3.1" pyarrow xmltodict blosc imgkit dipy
 mamba run -n warp pip install "git+ssh://git@github.com/uncbiag/uniGradICONLung.git"
 mamba run -n warp pip check
 
---
+++ tests/test_env.py
@@ -0,0 +1,16 @@
+import numpy
+import scipy
+import skimage
+import torch
+import nibabel
+import SimpleITK
+import meshio
+import pygalmesh
+import warp
+import vtk
+import pyvista
+import totalsegmentator
+import unigradicon
+
+print('PASS')
+
